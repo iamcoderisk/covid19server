e84cee59417ef5bc675792bfe2eb4dd1bb970759
iamcoderisk <ekeminyd@gmail.com>
2020-09-28 11:27:23 +0100

    build artifects hidden

--- routes/patients.js
+++ routes/patients.js
@@ -10,15 +10,15 @@ var  Request  =  require("../relevant-models/request");
 router.post('/list',(req,res)=>{
     // res.send("something happened")
     // // moment('01/12/2016', 'DD/MM/YYYY', true).format()
-    var a = moment(req.body.from,'DD-MM-YYYY',true).format(); 
-    var b = moment(req.body.to,'DD-MM-YYYY',true).format();
+    var a = moment(req.body.from,'MM/DD/YYYY',true).format(); 
+    var b = moment(req.body.to,'MM/DD/YYYY',true).format();
    
     let query = {created: {$gte: a, $lt: b} };
 
    Request
     .find(query)
-    .exec((err, account) => {
-        res.send(account)
+    .exec((err, data) => {
+        res.send([data])
     } 
     
     )
